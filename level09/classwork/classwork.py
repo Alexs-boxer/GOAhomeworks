cad902c6431b7b58ab218bd0d60361e43351cb69
Alexs-boxer <adanaiaalex08@gmail.com>
2025-05-22 07:18:33 +0400

    level 09

---
+++ level09/classwork/classwork.py
@@ -0,0 +1,5 @@
+i=1 
+while i > 1:
+    print(1)
+    i=i+1
+    
---
+++ level09/homework/homework.py
@@ -0,0 +1,20 @@
+i=1 
+while i > 1:
+    print(1)
+    i=i+1
+#უსასრულოდ განმეორება
+
+#  for loop -განმეორება სანამ არის true
+# while loop - უსასრულოდ განმეორება
+
+
+i=1
+while i > 1 :
+    print(1)
+    i=i+3
+
+
+i=10
+while i > 10 :
+    print(10)
+    i=i+5
