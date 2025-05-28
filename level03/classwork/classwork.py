b6f597634a28d1b7984130ab2b4361eb44122808
Alexs-boxer <adanaiaalex08@gmail.com>
2025-05-28 21:13:13 +0400

    corect

--- level03/classwork/classwork.py
+++ level03/classwork/classwork.py
@@ -1,1 +1,12 @@
- 
+favorite_food="burger"
+favorite_game="minecraft"
+
+favoriteBook="Harrypotter"
+favoriteSport="box"
+
+
+
+print(favorite_food)
+print(favorite_game)
+print(favoriteBook)
+print(favoriteSport)
---
+++ level11/classwork/classwork.py
@@ -0,0 +1,32 @@
+for i in range (10):
+    print (i)
+
+for i in range (2,21,2):
+    print(i)
+for i in range (1,n+1)
+
+i=2
+while 1<=20
+sum +=i
+
+
+i=1
+sum=0
+while i<-n
+sum+=i
+i+=1
+
+
+
+
+
+
+ 
+
+
+
+
+
+    
+
+
