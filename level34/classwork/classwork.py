1d66227bdc45b9f9ce857cce8dfcb281d62c0531
Alexs-boxer <adanaiaalex08@gmail.com>
2025-08-16 18:46:47 +0400

    level34

---
+++ level34/classwork/classwork.py
@@ -0,0 +1,8 @@
+#ფუნქცია გამოიყენება სტრიქონის გასაყოფად.
+frut = "apple,whatermallon,banana "
+eat = frut.split()
+print(eat)
+#გამოიყენება სტრინგების სიაში ელემენტების ერთმანეთთან შეერთებისთვის ერთ მთლიან სტრინგად.
+country= ["rusia", "uzbekistan", "georgia"]
+europ = " ".join(country)
+
